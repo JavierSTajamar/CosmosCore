ec4554bbe405aa4f55cae04e136546e4849dd981
JavierSR5597 <javier.sanchezrodriguez@tajamar365.com>
2022-04-29 16:12:51 +0200

    Initial commit

---
+++ Services/ICosmosDbService.cs
@@ -0,0 +1,16 @@
+﻿using CosmosCore.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace CosmosCore.Services
+{
+    public interface ICosmosDbService
+    {
+        Task<IEnumerable<Item>> GetItemsAsync(string query);
+        Task<Item> GetItemAsync(string id);
+        Task AddItemAsync(Item item);
+        Task UpdateItemAsync(string id, Item item);
+        Task DeleteItemAsync(string id);
+    }
+}
